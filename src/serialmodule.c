98c0f466f5a7b4eacaac750080266dd0a24c965c
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2024-12-09 23:16:50 +0700

    develop

--- src/serialmodule.c
+++ src/serialmodule.c
@@ -1,6 +1,19 @@
 #include "serialmodule.h"
 #include <stdio.h>
 #include <simplelog.h>
+#ifndef UNIX_LINUX
+#include <Windows.h>
+#else
+#endif
+
+//#ifndef UNIX_LINUX
+//#else
+//#endif
+
+
+static int
+serial_module_openport(void*);
+
 int
 serial_module_init(void *obj) {
 	fprintf(stdout, "hi!\n");
@@ -9,4 +22,20 @@ serial_module_init(void *obj) {
 int
 serial_module_close(void* obj) {
 	return 0;
+}
+
+int
+serial_module_openport(void* obj) {
+	int ret = 0;
+	int baudrate = 11520;
+	const char* portname = "\\\\.\\COM1";
+	do {
+#ifndef UNIX_LINUX
+		DCB dcb = {0};
+		HANDLE hCom = 0;
+#else
+#endif
+	} while (0);
+
+	return ret;
 }
