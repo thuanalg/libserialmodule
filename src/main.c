8ef162989163867e0c0a6f002ffdcd1096989745
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-01-17 17:25:46 +0700

    develop

--- src/main.c
+++ src/main.c
@@ -1,10 +1,25 @@
 #include "serialmodule.h"
+#include <stdio.h>
 
 
 
 int main(int argc, char *argv[]) {
 	SP_SERIAL_INPUT_ST obj;
+	FILE* fp = 0;
 	int myid = spserial_module_create(&obj);
+	while (1) {
+		fp = fopen("trigger_serial.txt", "r");
+		if (fp) {
+			break;
+		}
+		spl_sleep(5);
+	}
+	if (fp) {
+		fclose(fp);
+	}
+	if (myid > -1) {
+		spserial_module_setoff(myid);
+	}
 	return 0;
 }
 
