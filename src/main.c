8df4dc135489527a74e6cd5a9a90a50a9ab36579
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-02-23 14:03:00 +0700

    lx

--- src/main.c
+++ src/main.c
@@ -2,15 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int is_master = 0;
+#define __ISMASTER__			"--is_master="
 
 #define TESTTEST "1234567"
 int main(int argc, char *argv[]) {
+	int i = 0;
 	SP_SERIAL_INPUT_ST obj;
 	FILE* fp = 0;
 	int myid = 0;
 	int ret = 0;
 	char cfgpath[1024];
 	SPSERIAL_ARR_LIST_LINED* objId = 0;
+
+	for (i = 0; i < argc; ++i) {
+		if (strstr(argv[i], __ISMASTER__)) {
+			sscanf(argv[i], __ISMASTER__"%d", &is_master);
+			continue;
+		}
+	}
 
 #ifndef UNIX_LINUX
 	snprintf(cfgpath, 1024, "C:/z/serialmodule/win32/Debug/simplelog.cfg");
@@ -29,14 +39,16 @@ int main(int argc, char *argv[]) {
 	ret = spserial_inst_create(&obj, &myid);
 	ret = spserial_get_objbyid(myid, &objId, 0);
 	while (1) {
-		spl_console_log("-----------");
-		spserial_inst_write_to_port(objId->item, TESTTEST, sizeof(TESTTEST));
 		spl_sleep(1);
+		spl_console_log("-----------");
+		if (!is_master) {
+			spserial_inst_write_to_port(objId->item, TESTTEST, sizeof(TESTTEST));
+		}
 		fp = fopen("C:/z/serialmodule/win32/Debug/trigger_serial.txt", "r");
 		if (fp) {
 			break;
 		}
-		spl_sleep(1);
+		
 
 		
 	}
