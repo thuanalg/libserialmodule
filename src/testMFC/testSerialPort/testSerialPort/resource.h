5b799f7ce949a3e9f449b76e02bae0045a8f04bc
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-02-27 21:02:10 +0700

    lx

--- src/testMFC/testSerialPort/testSerialPort/resource.h
+++ src/testMFC/testSerialPort/testSerialPort/resource.h
@@ -11,6 +11,11 @@
 #define IDC_EDIT_COMPORT                1000
 #define IDC_BUTTON1                     1001
 #define IDC_BUTTON_msg                  1001
+#define IDC_BUTTON_INIT_MODULE          1002
+#define IDC_BUTTON_STOP_MODULE          1003
+#define IDC_BUTTON3                     1004
+#define IDC_BUTTON_ADD                  1004
+#define IDC_EDIT2                       1005
 
 // Next default values for new objects
 // 
@@ -18,7 +23,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
