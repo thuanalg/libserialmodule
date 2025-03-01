eaf036cd259928c8272de69072ebbe44822cc113
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-03-01 20:30:47 +0700

    lx

--- src/serialmodule.c
+++ src/serialmodule.c
@@ -15,7 +15,13 @@
     #include <fcntl.h> /* For O_* constants */
     #include <errno.h>
     #include <termios.h>
-
+    #include <stdlib.h> 
+    #include <unistd.h> 
+    #include <string.h> 
+    #include <sys/types.h> 
+    #include <sys/socket.h> 
+    #include <arpa/inet.h> 
+    #include <netinet/in.h> 
     #ifdef __TRUE_LINUX__
         #include <sys/epoll.h>
     #else
@@ -38,6 +44,8 @@
             ;}\
         }
 #else
+    #define SPSR_MAXLINE                1024
+    #define SPSR_PORT                   10024
 #endif
 
 /*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
@@ -1009,6 +1017,49 @@ int spserial_inst_write_data(int idd, char* data, int sz) {
 #ifndef UNIX_LINUX
 #else
     int spserial_init_trigger(void* obj) { 
+        int ret = 0;
+        int sockfd = 0;
+        int n = 0;
+        socklen_t len = 0;
+        char buffer[SPSR_MAXLINE];
+        const char* hello = "Hello from server";
+        struct sockaddr_in servaddr, cliaddr;
+
+        // Creating socket file descriptor 
+        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            perror("socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        memset(&servaddr, 0, sizeof(servaddr));
+        memset(&cliaddr, 0, sizeof(cliaddr));
+
+        // Filling server information 
+        servaddr.sin_family = AF_INET; // IPv4 
+        servaddr.sin_addr.s_addr = INADDR_ANY;
+        servaddr.sin_port = htons(SPSR_PORT);
+
+        // Bind the socket with the server address 
+        if (bind(sockfd, (const struct sockaddr*)&servaddr,
+            sizeof(servaddr)) < 0)
+        {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+
+        
+        
+
+        len = sizeof(cliaddr);  //len is value/result 
+
+        n = recvfrom(sockfd, (char*)buffer, MAXLINE,
+            MSG_WAITALL, (struct sockaddr*)&cliaddr,
+            &len);
+        buffer[n] = '\0';
+        printf("Client : %s\n", buffer);
+        sendto(sockfd, (const char*)hello, strlen(hello),
+            MSG_CONFIRM, (const struct sockaddr*)&cliaddr,
+            len);
         return 0;
     }
 /*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
