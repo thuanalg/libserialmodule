c29d3095fed0daba1a04618b94a174012beaf46c
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-03-22 22:27:36 +0700

    test mach

--- src/xcode/test_libserialmodule/test_libserialmodule/main.m
+++ src/xcode/test_libserialmodule/test_libserialmodule/main.m
@@ -6,11 +6,14 @@
 //  Copyright © 2025 salmon. All rights reserved.
 //
 
+#include <serialmodule.h>
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
     }
-    return NSApplicationMain(argc, argv);
+    int ret = 0;
+    ret = NSApplicationMain(argc, argv);
+    return ret;
 }
