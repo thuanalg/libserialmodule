84084d6fdf88305e14d90d14bd889ade0c9429cc
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-03-22 19:53:20 +0700

    add test xcode

---
+++ src/xcode/test_libserialmodule/test_libserialmodule/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  test_libserialmodule
+//
+//  Created by Thuan T.Nguyen on 3/22/25.
+//  Copyright © 2025 salmon. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
