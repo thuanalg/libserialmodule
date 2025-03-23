825ff0e5f8aa6ef13b4dfc27b2cd560870b1aa71
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-03-23 22:48:20 +0700

    mach

--- src/xcode/test_libserialmodule/test_libserialmodule/main.m
+++ src/xcode/test_libserialmodule/test_libserialmodule/main.m
@@ -6,11 +6,22 @@
 //  Copyright © 2025 salmon. All rights reserved.
 //
 
+#include <serialmodule.h>
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
     }
-    return NSApplicationMain(argc, argv);
+    int ret = 0;
+    ret = spl_init_log((char*)"/Users/ntthuan/Documents/libserialmodule/src/mach/simplelog.cfg");
+    if(ret) {
+        exit(1);
+    }
+    
+    ret = NSApplicationMain(argc, argv);
+    
+    spl_finish_log();
+    return ret;
 }
+	
