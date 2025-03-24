4b2f9106c917c20fd5fefc5851786c3723b2885b
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-03-24 13:28:27 +0700

    mach

--- src/xcode/test_libserialmodule/test_libserialmodule/main.m
+++ src/xcode/test_libserialmodule/test_libserialmodule/main.m
@@ -4,7 +4,7 @@
 //
 //  Created by Thuan T.Nguyen on 3/22/25.
 //  Copyright © 2025 salmon. All rights reserved.
-//
+
 
 #include <serialmodule.h>
 #import <Cocoa/Cocoa.h>
@@ -14,10 +14,12 @@ int main(int argc, const char * argv[]) {
         // Setup code that might create autoreleased objects goes here.
     }
     int ret = 0;
-    ret = spl_init_log((char*)"/Users/ntthuan/Documents/libserialmodule/src/mach/simplelog.cfg");
+    ret = spl_init_log((char*)"/Users/ntthuan/Library/Developer/Xcode/DerivedData/"\
+                       "test_libserialmodule-bgxiutcbhqcyifdcinmepeuqethd/Build/Products/"\
+                       "Debug/test_libserialmodule.app/Contents/Resources/simplelog.cfg");
     if(ret) {
         exit(1);
-    }
+    }	                
     
     ret = NSApplicationMain(argc, argv);
     
