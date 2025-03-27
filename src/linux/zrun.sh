b2ce3c7163668e8dbd374e7661d3ef7b75311735
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-03-27 22:23:15 +0700

    Prepare for alpha release.

--- src/linux/zrun.sh
+++ src/linux/zrun.sh
@@ -1,3 +1,3 @@
 mv trigger_serial.txt trigger_serial.txt_
 export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
-./test_main --is_baudrate=9600 --is_cfg=simplelog.cfg --is_port=/dev/ttyUSB0
+./test_main --is_baudrate=115200 --is_cfg=simplelog.cfg --is_port=/dev/ttyUSB0,/dev/ttyUSB1
--- src/mach/zrun.sh
+++ src/mach/zrun.sh
@@ -1,3 +1,6 @@
 mv trigger_serial.txt trigger_serial.txt_
 export DYLD_LIBRARY_PATH=./:$DYLD_LIBRARY_PATH
-./test_main --is_baudrate=9600 --is_cfg=simplelog.cfg --is_port=/dev/tty.Plser,/dev/tty.Plser1
+#export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
+./test_main --is_baudrate=115200 --is_cfg=simplelog.cfg --is_port=/dev/tty.Plser,/dev/tty.Plser1
+#./test_main --is_baudrate=115200 --is_cfg=simplelog.cfg --is_port=/dev/ttyUSB0,/dev/ttyUSB1
+
