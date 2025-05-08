569fc8e82c729ffb88b92a83fa0767f1a889c2e7
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-05-08 13:46:51 +0700

    Test Mac-64bit vs Win 32 bit.

--- src/mach/zrun.sh
+++ src/mach/zrun.sh
@@ -1,5 +1,6 @@
 mv trigger_serial.txt trigger_serial.txt_
-export DYLD_LIBRARY_PATH=./:$DYLD_LIBRARY_PATH
+#You need "export" correctly. it depends on your setting, system.
+export DYLD_LIBRARY_PATH=./:$DYLD_LIBRARY_PATH:/usr/local/thuannt
 #export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
 ./test_main --is_baudrate=115200 --is_cfg=simplelog.cfg --is_port=/dev/tty.Plser,/dev/tty.Plser1
 #./test_main --is_baudrate=115200 --is_cfg=simplelog.cfg --is_port=/dev/ttyUSB0,/dev/ttyUSB1
