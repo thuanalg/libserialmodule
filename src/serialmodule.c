96227711e77cb72493a27a0f4ce42d9db82bf25e
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-07-01 15:41:01 +0700

    x1

--- src/serialmodule.c
+++ src/serialmodule.c
@@ -17,6 +17,7 @@
 		<2025-May-06>
 		<2025-May-13>
 		<2025-May-20>
+		<2025-Jul-01>		
 * Decription:
 *		The (only) main header file to export
 		5 APIs: [spsr_module_init, spsr_module_finish, spsr_inst_open,
@@ -169,6 +170,9 @@ spsr_win32_read(SPSR_INFO_ST *p, DWORD *bytesWrite, OVERLAPPED *olReadWrite,
 typedef void *(*SPSR_THREAD_ROUTINE)(void *);
 static int
 spsr_init_trigger(void *);
+
+static void 
+set_rts_dtr(int fd, char rts_dtr);
 
 static int
 spsr_px_write(SPSR_GENERIC_ST *item, SPSR_GENERIC_ST *evt);
@@ -2147,7 +2151,12 @@ spsr_px_add(SPSR_GENERIC_ST *item, SPSR_GENERIC_ST *evt, int epollfd)
 			l = strlen(input->port_name);
 			ret = spsr_open_fd(
 			    input->port_name, input->baudrate, &fd);
-
+			if(input->rts || input->dtr) {
+				char rts_dtr = 0;
+				rts_dtr |= (!!input->rts) ? 0x02 : 0x00;
+				rts_dtr |= (!!input->dtr) ? 0x02 : 0x00;
+				set_rts_dtr(fd, rts_dtr);
+			}
 			if (ret) {
 				if (input->cb_evt_fn) {
 					spsr_invoke_cb(
@@ -3014,7 +3023,30 @@ spsr_clear_hash()
 }
 
 /*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
+void set_rts_dtr(int fd, char rts_dtr)
+{
+    int status;
+    if (ioctl(fd, TIOCMGET, &status) == -1) {
+        spsr_err("ioctl TIOCMGET");
+        return;
+    }
+	if(0x01 & rts_dtr) {
+    	status |= TIOCM_RTS; 
+		spsr_all("RTS was found.\n");
+	}
+	if(0x02 & rts_dtr) {
+    	status |= TIOCM_DTR;
+		spsr_all("DTR was found.\n");
+	}
 
+    if (ioctl(fd, TIOCMSET, &status) == -1) {
+        spsr_err("ioctl TIOCMSET");
+        return;
+    }
+
+    spsr_all("RTS và DTR were on already.\n");
+}
+/*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
 int
 spsr_open_fd(char *port_name, int baudrate, int *outfd)
 {
